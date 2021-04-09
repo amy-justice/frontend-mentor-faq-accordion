36da1746205b8c641dbcbbd24192e60e6f2eb198
Amy <aljustice25@gmail.com>
2021-04-09 19:17:58 +1200

    mobile view update

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ let acc = document.getElementsByClassName('accordion-question');
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        for(j = 0; j < acc.length; j++) {
-            acc[j].nextElementSibling.style.maxHeight = null;
-            acc[j].style.fontWeight = '400';
-        }
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-        this.style.fontWeight = '400';
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        this.style.fontWeight = '700';
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+      this.style.fontWeight ='400'
+    } else {
+      let active = document.querySelectorAll(".accordion-faq .accordion-question.active");
+      for(let j = 0; j < active.length; j++){
+        active[j].classList.remove("active");
+        active[j].style.fontWeight = '400'
+        active[j].nextElementSibling.style.maxHeight = null;
       }
+      this.classList.toggle("active");
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      this.style.fontWeight = '700'
+    }
     });
 }
